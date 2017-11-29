20222530ba72b23abb73ecbd6fe3042624d4946a
vagrant <vagrant@homestead>
2017-11-29 08:19:30 +0000

    Topic index page

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Topic;
 
 class TopicsController extends Controller
 {
@@ -13,7 +14,9 @@ class TopicsController extends Controller
      */
     public function index()
     {
-        //
+        $topics = Topic::with('category', 'user')->paginate(30);
+
+        return view('topics.index', compact('topics'));
     }
 
     /**

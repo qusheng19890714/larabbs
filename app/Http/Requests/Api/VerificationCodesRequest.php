f129c9bc661c497749d7cec775660c71c73439b1
vagrant <vagrant@homestead>
2018-02-07 02:56:33 +0000

    user store and verificationCode store

---
+++ app/Http/Requests/Api/VerificationCodesRequest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Requests\Api;
+
+use Dingo\Api\Http\FormRequest;
+
+class VerificationCodesRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'phone'=> 'required|regex:/^1[34578]\d{9}$/|unique:users',
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'phone.regex'=>'格式错误',
+        ];
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -21,18 +21,11 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 $api = app('Dingo\Api\Routing\Router');
 
-$api->version('v1', function($api) {
+$api->version('v1', [
+    'namespace'=>'App\Http\Controllers\Api',
+], function($api){
+    //短信验证码
+    $api->post('verificationCodes', 'VerificationCodesController@store')
+        ->name('api.verificationCodes.store');
 
-    $api->get('version', function(){
-
-        return response('this is version v1');
-    });
-});
-
-$api->version('v2', function($api) {
-
-    $api->get('version', function(){
-
-        return response('this is version v2');
-    });
 });
